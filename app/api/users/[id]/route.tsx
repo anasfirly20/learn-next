01fb279d42cb3390d2aeed3f34c301edde2de8ac
Anas Firly <anasfirly20@gmail.com>
2023-09-15 15:30:44 +0400

    adding put req

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -8,6 +8,19 @@ export function GET(request: NextRequest, { params }: TProps) {
   if (params.id > 10) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
-
   return NextResponse.json({ id: 1, name: "Firly" });
+}
+
+export async function PUT(request: NextRequest, { params }: TProps) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required!" }, { status: 404 });
+  }
+
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found." }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name });
 }

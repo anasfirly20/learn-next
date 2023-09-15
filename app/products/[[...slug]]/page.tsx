54248902168a8a794ebfb17bd98a527e5dff5866
Anas Firly <anasfirly20@gmail.com>
2023-09-15 09:12:58 +0400

    ading searchParams

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 type TProps = {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 };
 
-export default function ProductsPage({ params: { slug } }: TProps) {
+export default function ProductsPage({
+  params: { slug },
+  searchParams: { sortOrder },
+}: TProps) {
   return (
     <div>
       <h1>Products page: {slug}</h1>
+      <h1>Sort order: {sortOrder}</h1>
     </div>
   );
 }

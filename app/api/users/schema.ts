76136a4cd223a08014a9c93bb759124878b97e0c
Anas Firly <anasfirly20@gmail.com>
2023-09-16 17:43:55 +0400

    adding create

--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 const schema = z.object({
   name: z.string().min(3),
-  price: z.number().positive(),
+  email: z.string().email(),
+  // price: z.number().positive(),
 });
 
 export default schema;

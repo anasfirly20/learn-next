bb63d1fdcc4b1aa7a8a67a7ed5118381a50be83a
Anas Firly <anasfirly20@gmail.com>
2023-09-16 17:34:49 +0400

    adding findUnique

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,16 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
 type TProps = {
-  params: { id: number };
+  params: { id: string };
 };
 
 // GET by Id
-export function GET(request: NextRequest, { params }: TProps) {
-  if (params.id > 10) {
+export async function GET(request: NextRequest, { params }: TProps) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
-  return NextResponse.json({ id: 1, name: "Firly" });
+  return NextResponse.json(user);
 }
 
 // PUT by Id
@@ -22,18 +29,18 @@ export async function PUT(request: NextRequest, { params }: TProps) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (params.id > 10) {
-    return NextResponse.json({ error: "User not found." }, { status: 404 });
-  }
+  // if (params.id > 10) {
+  //   return NextResponse.json({ error: "User not found." }, { status: 404 });
+  // }
 
   return NextResponse.json({ id: 1, name: body.name });
 }
 
 // DELETE by Id
 export function DELETE(request: NextRequest, { params }: TProps) {
-  if (params.id > 10) {
-    return NextResponse.json({ error: "User not found." }, { status: 404 });
-  }
+  // if (params.id > 10) {
+  //   return NextResponse.json({ error: "User not found." }, { status: 404 });
+  // }
 
   return NextResponse.json({});
 }

c693450606da02fb10da796ec56b2f365bad1cd8
Anas Firly <anasfirly20@gmail.com>
2023-09-16 17:49:59 +0400

    adding update

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -29,11 +29,23 @@ export async function PUT(request: NextRequest, { params }: TProps) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  // if (params.id > 10) {
-  //   return NextResponse.json({ error: "User not found." }, { status: 404 });
-  // }
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  if (!user) {
+    return NextResponse.json({ error: "User not found." }, { status: 404 });
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 // DELETE by Id

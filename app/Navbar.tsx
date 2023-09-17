777cf47c1a7e4fdee45df485b06ae27d06279f41
Anas Firly <anasfirly20@gmail.com>
2023-09-17 12:05:42 +0400

    accessing session on the client

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,13 +1,22 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
+  const { status, data: session } = useSession();
+
   return (
     <nav className="flex w-full sticky top-0 justify-between px-[20vw] py-[1vh] bg-cyan-700">
       <Link href="/">Home</Link>
       <Link href="/users">Users</Link>
       <Link href="/upload">Upload</Link>
-      <Link href="/api/auth/signin">Sign In</Link>
+      {status === "loading" && <p>loading...</p>}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Sign In</Link>
+      )}
     </nav>
   );
 }

3c07bbe44083cda81363a9509ee28bf2961f0bdb
Anas Firly <anasfirly20@gmail.com>
2023-09-21 21:54:10 +0400

    optimizing images

--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const schema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
-  // price: z.number().positive(),
 });
 
 export default schema;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -14,6 +15,17 @@ export default async function Home() {
       </div>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <section className="relative h-screen">
+        <Image
+          src="https://bit.ly/react-cover"
+          alt="Coffee"
+          fill
+          className="object-cover"
+          sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+          quality={100}
+          priority={true}
+        />
+      </section>
     </main>
   );
 }
